913f48b6f00e31170a0ba5602984ee58b38341bd
mkanderson71 <mkanderson71@gmail.com>
2022-06-17 10:25:09 -0400

    add error handling, run imgurl thru pillow

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import random
 import os
 import requests
 import datetime
+from PIL import Image
+from io import BytesIO
 from flask import Flask, render_template, abort, request
 from QuoteEngine.importer import Importer
 from QuoteEngine.quotemodel import QuoteModel
@@ -52,15 +54,38 @@ def meme_post():
     image_url = request.form['image_url']
     body = request.form['body']
     author = request.form['author']
+    try:
+        r = requests.get(image_url)
+        r.raise_for_status()
+    except (requests.HTTPError,
+            requests.RequestException,
+            requests.ConnectionError,
+            requests.URLRequired) as e:
+        print('Could not load image file, ' +
+              'please check the URL and try again.' +
+              f'{e}')
+        return render_template('meme_error.html')
 
-    r = requests.get(image_url)
-    img_ext = image_url.rsplit('.')[-1].strip()
-    temp_img = './tmp/img_' + \
+    temp_img = './static/tmp_' + \
                datetime.datetime.now().strftime("%y%m%d_%H%M%S") + \
-               f'.{img_ext}'
+               f'.jpg'
 
-    with open(temp_img, 'wb') as img:
-        img.write(r.content)
+    # opening and saving the response stream with pillow
+    # adds additional insurance response is a valid image
+    try:
+        im = Image.open(BytesIO(r.content))
+    except OSError:
+        print('The remote server did not return a valid image, ' +
+              'please check the URL and try again.' +
+              f'{e}')
+        return render_template('meme_error.html')
+    try:
+        im.save(temp_img)
+    except OSError:
+        print('Unable to save image from this URL, ' +
+              'please try another address.' +
+              f'{e}')
+        return render_template('meme_error.html')
 
     if not body or not author:
         imp = Importer()
